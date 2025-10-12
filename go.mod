6343f4585952273f9e14d6a498962a6b3890a9d8
Juan Cruz Estevez <72108526+juancruzestevez@users.noreply.github.com>
2025-10-12 16:13:10 -0300

    main config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/juancruzestevez/auth-service
 
-go 1.25.1
+go 1.23
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, using default values")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
